ae6f49e7cc153ed381fd4a502ac79f8dbacd9159
Artem Skripka <therewasadrizzle@gmail.com>
2018-11-01 00:43:20 +0400

    add specs for Spree::Admin::ProductImportsController

---
+++ spec/controllers/spree/admin/product_imports_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Spree::Admin::ProductImportsController do
+  render_views
+
+  let(:admin) { create :admin_user }
+
+  before do
+    sign_in(admin, scope: :spree_user)
+  end
+
+  it 'gets #index' do
+    spree_get :index
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'gets #new' do
+    spree_get :new
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'posts #create' do
+    product_import_attrs = attributes_for(:product_import)
+    expect{
+      spree_post :create, { product_import: product_import_attrs }
+    }.to change{ ProductImport.count }.by(1)
+  end
+end
